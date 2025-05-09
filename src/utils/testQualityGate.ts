80aa5cb99504aa80c724e7a05b2df7089f823afe
zohaibterminator <zohaibsaqib803@gmail.com>
2025-05-10 04:46:29 +0500

    add bad code

---
+++ src/utils/testQualityGate.ts
@@ -0,0 +1,18 @@
+// file: src/testQualityGate.js
+
+function sayHello() {
+    console.log("Hello");
+}
+
+function sayHelloAgain() {
+    console.log("Hello");
+}
+
+function longFunction() {
+    for (let i = 0; i < 100; i++) {
+        console.log("Line " + i);
+    }
+}
+
+const userInput = "2 + 2";
+eval(userInput);
